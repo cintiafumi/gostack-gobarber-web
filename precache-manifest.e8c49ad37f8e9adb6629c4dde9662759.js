29d1f2560467ae3928620aa204a346b76d5f950d
Cíntia Fumi <cintiafumi@gmail.com>
2020-11-20 10:11:29 -0300

    Updates

---
+++ precache-manifest.e8c49ad37f8e9adb6629c4dde9662759.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3796cc8f392598ac53343be208ba7fcb",
+    "url": "/gostack-gobarber-web/index.html"
+  },
+  {
+    "revision": "84df90ac8b5d6231dd1c",
+    "url": "/gostack-gobarber-web/static/css/2.c6231af7.chunk.css"
+  },
+  {
+    "revision": "84df90ac8b5d6231dd1c",
+    "url": "/gostack-gobarber-web/static/js/2.0a20cf0a.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/gostack-gobarber-web/static/js/2.0a20cf0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23231212ae1dd9450748",
+    "url": "/gostack-gobarber-web/static/js/main.43df5149.chunk.js"
+  },
+  {
+    "revision": "f9434a9269d695bb21a9",
+    "url": "/gostack-gobarber-web/static/js/runtime-main.4242b2a8.js"
+  },
+  {
+    "revision": "a49b07ec538713cf0e3910693544c7be",
+    "url": "/gostack-gobarber-web/static/media/logo.a49b07ec.svg"
+  },
+  {
+    "revision": "6eac314f78fe224c4d769fb646523773",
+    "url": "/gostack-gobarber-web/static/media/sign-in-background.6eac314f.png"
+  },
+  {
+    "revision": "c4d8f988b9c6499d56702a398b2f3d8b",
+    "url": "/gostack-gobarber-web/static/media/sign-up-background.c4d8f988.png"
+  }
+]);
